7bed7cb3a30c5f096ab43ea30301eef5eb67d777
JuanDiegoAvila <60203575+JuanDiegoAvila@users.noreply.github.com>
2021-02-21 17:38:16 -0600

    .

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,29 @@
+import java.io.*;
+import java.util.Scanner;
+
+
 public class Main {
     public static void main(String[]args){
 
+        String datos = "";
+        try {
+            Scanner input = new Scanner(new File("datos.txt"));
+            int cont = 0;
+            while (input.hasNextLine()) {
+                datos += input.nextLine();
+            }
+            input.close();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
 
+        System.out.println(datos);
+
+    }
+
+    private String postFix(String infix){
+
+        return "";
+        //holaaaaaaaaa
     }
 }
