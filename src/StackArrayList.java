fd3247764d2bbc166ff1a56bed6622cd66b2c363
EstebanAG1005 <60299338+EstebanAG1005@users.noreply.github.com>
2021-02-21 18:08:11 -0600

    Update StackArrayList.java

--- src/StackArrayList.java
+++ src/StackArrayList.java
@@ -1,15 +1,59 @@
 import java.util.ArrayList;
+  
+public class StackArrayList<E> implements Stack<E> {
 
-public class StackArrayList {
-<<<<<<< HEAD
-<<<<<<< HEAD
-    System.out.println("3");
-=======
+    public ArrayList<E> array;
+
+    public StackArrayList(){
+        this.array = new ArrayList<E>();
+    }
     
-    
->>>>>>> 1ddcf654871d86d61e6ba371f2f797861267186a
-=======
-    
-    
->>>>>>> 1ddcf654871d86d61e6ba371f2f797861267186a
-}
+    //Ingresa Valor al Stack
+    @Override
+    public void push(E item) {
+
+        array.add(item);
+    }
+    //Saca elemento del Stack
+    @Override
+    public E pop() {
+        //El Stack no debe de estar vacio, por eso se chequea para ver si lo esta 
+        if(array.isEmpty())
+        {
+            return null;
+        }
+        else
+        {
+            //sca el ultimo elemento
+            E v = array.get(array.indexOf(array.get(array.size() - 1)));
+            array.remove(v);
+            return v;
+        }
+    }
+    //Ultimo elemento del Stack
+    @Override
+    public E peek() {
+
+        //El Stack no debe de estar vacio, por eso se chequea para ver si lo esta 
+        if(array.isEmpty())
+        {
+            return null;
+        }
+        else{
+            return array.get(array.indexOf(array.get(0)));
+        }
+    }
+     //Ver si esta vacio el Stack o no
+     @Override
+     public boolean empty() {
+ 
+         return array.isEmpty();
+     }
+ 
+     //Tamaño del Stack
+     @Override
+     public int size() {
+ 
+         return array.size();
+     }
+ }
